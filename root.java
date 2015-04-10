859b4c3de812733a87def74ae23774ff8ef8fdd1
mano91 <mano.c247@gmail.com>
2015-04-10 12:00:11 +0530

    Create root.java

---
+++ root.java
@@ -0,0 +1,13 @@
+class Root{
+void GetIn(){
+ system.out.println("I am in Root");
+ }
+}
+
+public class Base {
+public static void main(String args []) {
+ Root root = new Root();
+ root.GetIn();
+ }
+} 
+ 
